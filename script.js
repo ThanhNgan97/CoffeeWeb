8b79baca1963f69d7dd4c5ff97dbd342c3c7a904
ThanhNgan97 <nguyenthanhngan09072002@gmail.com>
2025-04-28 19:34:31 +0700

    swiper

--- script.js
+++ script.js
@@ -9,3 +9,40 @@ menuOpenButton.addEventListener("click", ()=>{
 
 menuCloseButton.addEventListener("click", () => 
     menuOpenButton.click());
+
+
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween:25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+    //Ressponsive breakpoiter
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+
+        768: {
+            slidesPerView: 2
+        },
+
+        1024: {
+            slidesPerView: 3
+        },
+
+    }
+  });
